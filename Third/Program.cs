49911c1814772f7b5ded59f8b05b93566039313b
Meh <mehonoshin90@gmail.com>
2021-02-04 20:36:56 +0800

    Urok

--- Third/Program.cs
+++ Third/Program.cs
@@ -6,7 +6,7 @@ namespace Third
     {
         static void Main(string[] args)
         {
-            Task4();
+            Task10();
 
         }
 
@@ -95,5 +95,93 @@ namespace Third
             }
 
         }
+
+        static void Task5()
+        {
+            int test = 1;
+            Console.WriteLine(test);
+            //test++;   // Инкримент прибавляет 1 декримент вычитает 1 "++|--" В зависимости от положения инкримента выполняет до вывода или после.
+            Console.WriteLine(test++);
+            Console.WriteLine(test);
+            Console.WriteLine(++test);
+
+        }
+
+        static void Task6()
+        {
+            int zero = 0;
+            while (zero < 5)   //Объявлениие одного из видов цикла.
+            {
+                zero++;
+                Console.WriteLine($"{zero}, V cikle!");
+            }
+            Console.WriteLine($"{zero}, Posle cikla!");
+
+        }
+        static void Task7()
+        {
+            int zero = 5;
+            do // do один раз заходит в тело цикла и выполняет его
+            {
+                zero++;
+                Console.WriteLine($"{zero}, V cikle!");
+            } while (zero < 5);
+            Console.WriteLine($"{zero}, Posle cikla!");
+
+        }
+        static void Task8()
+        {
+            int zero = 5;
+            switch (zero) //Аналог if else if проверяет значения
+            {
+                case 0: //Проверка переменной на значение
+                    Console.WriteLine("case0!");
+                    break;
+                case 1:
+                    Console.WriteLine("case1!");
+                    break;
+                case 2:
+                    Console.WriteLine("case2!");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("case 3 or 4!");
+                    break;
+                case 5:
+                    Console.WriteLine("case5!");
+                    goto case 2;
+                default: // Необязателен
+                    Console.WriteLine("Default!");
+                    break;
+
+
+            }
+
+        }
+        static void Task9()
+        {//for задаёт количество повторений цикла условием
+            for (int zero = 0; zero < 5; zero++)
+            {
+                Console.WriteLine("{zero}, V cikle!");
+            }
+        }
+        static void Task10()
+        {
+            string Hi = "Hello world!";
+            for (int zero = 0; zero < Hi.Length; zero++)
+            {
+                if(Hi [zero] == ' ')
+                {
+                    break; //Прервает текущий цикл.
+                }
+                if(Hi  [zero] == 'l')
+                {
+                    continue; //Заставляет цикл перейти к следующей итерации
+                }
+                Console.WriteLine(Hi [zero]);
+            }
+        }
+
+
     }
 }
