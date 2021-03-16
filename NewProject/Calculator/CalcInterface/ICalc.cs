d055afcae9f9fe0f9a73d0bf23b8a81a9ea1a9df
Meh <mehonoshin90@gmail.com>
2021-03-16 20:20:30 +0800

    1

---
+++ NewProject/Calculator/CalcInterface/ICalc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CalcInterface
+{
+    public interface ICalc
+    {
+        double State
+        {
+            get;
+        }
+        string[] GetAvailableOperations();
+        void SetOperation(string operation);
+        void SetOperand(double operand);
+
+    }
+}
---
+++ NewProject/Calculator/SimpleCalc/SimpleCalc.cs
@@ -0,0 +1,46 @@
+﻿using CalcInterface;
+using System;
+
+namespace SimpleCalc
+{
+    public class SimpleCalc : ICalc
+    {
+        private string _operation = null;
+        public double State //Автосвойство
+        {
+            get;
+            private set;
+        }
+
+
+        public string[] GetAvailableOperations()
+        {
+            return new string[] { "+", "-", "*", "/", "C" };
+        }
+
+        public void SetOperand(double operand)
+        {
+            switch (_operation)
+            {
+                case null:
+                    State = operand;
+                    break;
+                case "+":
+                    break;
+                case "-":
+                    break;
+                case "*":
+                    break;
+                case "/":
+                    break;
+                case "C":
+                    break;
+            }
+        }
+
+        public void SetOperation(string operation)
+        {
+            _operation = operation;
+        }
+    }
+}
